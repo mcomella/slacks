2ea1751ffaa775dcb43662e968f11d1ec5a15f57
Michael Comella <michael.l.comella@gmail.com>
2013-02-15 01:04:48 -0500

    Add method to get permanent schedule.

--- slacks.py
+++ slacks.py
@@ -15,9 +15,14 @@ Written in Spring 2013 by Michael Comella (mcomella).
 
 CONSULT_DIR = '/admin/consult/'
 SCHED_DIR = CONSULT_DIR + 'data/sched/'
+PERM_SCHED_FILE = SCHED_DIR + 'sched.perm'
 
 META_FILE = SCHED_DIR + 'sched.meta' # Metadata file associated with csched.
 START_WEEK = 0 # The index of the initial consulting week.
+
+SHIFT_START_HOUR = 9 # 9am
+SHIFT_END_HOUR = 28 # 4am (during reading period)
+SHIFT_RANGE = SHIFT_END_HOUR - SHIFT_START_HOUR
 
 ERR_LOGTAG = 'depantsed! -'
 
@@ -25,9 +30,12 @@ from datetime import datetime
 import argparse
 import sys
 
+pp = PrettyPrinter(indent=4)
+
 def main():
     args = set_and_parse_args()
     metadata = get_metadata()
+    perm_sched = get_perm_sched()
 
 def set_and_parse_args():
     """Sets up, parses and returns any command line arguments.
@@ -62,6 +70,7 @@ def get_metadata():
         if len(file_lines) is not 1:
             exit('get_metadata', 'Unknown format in "' + META_FILE + '".')
         # string.split(sep=whitespace, maxsplit=3).
+        # TODO: Just give file to Metadata class.
         return Metadata(*tuple(file_lines[0].split(None, 3)))
 
 class Metadata:
@@ -89,8 +98,24 @@ class Metadata:
         self.start_date = datetime(date_list[2], date_list[0],
                 date_list[1]) # YMD.
 
+        # TODO: Make sure this actually returns the correct week.
         date_delta = datetime.now() - self.start_date
         self.cur_week = date_delta.days / 7 + START_WEEK
+
+def get_perm_sched():
+    # TODO: Add doc.
+    sched_arr = [[None] * 7 for i in range(SHIFT_RANGE)] # Zero array.
+    with open(PERM_SCHED_FILE) as f:
+        for line in f:
+            tokens = line.split()
+            shift = tokens[0]
+            login = tokens[1]
+            # tokens[2], if it exists, is the sub requester, which can be
+            # blissfully ignored.
+            hour_index = ord(shift[0]) - ord('a')
+            day_index = int(shift[1]) - 1 # 0 is Monday.
+            sched_arr[hour_index][day_index] = login
+    return sched_arr
 
 def exit(func_name, message):
     sys.exit(ERR_LOGTAG + ' ' + func_name + '(): ' + message)
